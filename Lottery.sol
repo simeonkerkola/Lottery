17b4dadf85272e573ebdd17e36a4f265709c8f4c
sssmi <simeon.kerkola@gmail.com>
2022-03-12 09:48:32 +0200

    Add require statements

--- Lottery.sol
+++ Lottery.sol
@@ -11,11 +11,18 @@ contract Lottery {
     }
 
     receive() external payable {
+
+        // User must send 0.1ETH min, 100000000000000000 wei
+        // Require doesn't consume gas because there's no code before it
+        require(msg.value == 0.1 ether);
+
         // Convert plain address to payable one
         players.push(payable(msg.sender));
     }
 
     function getBalance() public view returns (uint) {
+        // On]y ownew can read the balance
+        require(msg.sender == owner);
         return address(this).balance;
     }
 }
